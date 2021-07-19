39631e15825541fc70b40d52f75fb0f58c93966f
Yesenia Hernandez Hernandez <jeessee.hndz@gmail.com>
2021-07-19 18:56:47 -0500

    comit inicial

--- src/test/java/navegation/Navegation.java
+++ src/test/java/navegation/Navegation.java
@@ -3,7 +3,7 @@ package navegation;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
-import sustainability.SustainabilityNavegation;
+import signup.SignUpNavegation;
 
 
 public class Navegation {
@@ -11,7 +11,7 @@ public class Navegation {
 
     public static Performable theRequestPage() {
         return Task.where("{0} opens the home page",
-                Open.browserOn().the(SustainabilityNavegation.class));
+                Open.browserOn().the(SignUpNavegation.class));
 
 
     }
